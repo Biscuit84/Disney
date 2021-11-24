e103372eb147d13d609a6cc573e9dd7e57e574ac
anais <anais.bruno26@gmail.com>
2021-11-24 14:37:30 +0100

    query boutique

--- disney-project/src/main/java/disney/repo/IBoutiqueRepo.java
+++ disney-project/src/main/java/disney/repo/IBoutiqueRepo.java
@@ -1,10 +1,17 @@
 package disney.repo;
 
-import org.springframework.data.jpa.repository.JpaRepository;
+import java.util.List;
 
-import disney.classe.Admin;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+
 import disney.classe.Boutique;
 
 public interface IBoutiqueRepo extends JpaRepository<Boutique,Long> {
 
+	@Query("select distinct b from Boutique b left join fetch b.personnages p")
+	List<Boutique> findAllWithPersonnages();
+	
+	
+	
 }
