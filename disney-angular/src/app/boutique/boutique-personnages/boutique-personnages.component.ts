8304b194d5d47a07a6de6baed7ea94a1c05c614e
anais <anais.bruno26@gmail.com>
2021-12-02 17:26:38 +0100

    début boutique perso

--- disney-angular/src/app/boutique/boutique-personnages/boutique-personnages.component.ts
+++ disney-angular/src/app/boutique/boutique-personnages/boutique-personnages.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { Personnage } from 'src/model';
+import { BoutiquePersoHttpService } from './boutique-perso-http.service';
 
 @Component({
   selector: 'app-boutique-personnages',
@@ -7,9 +9,42 @@ import { Component, OnInit } from '@angular/core';
 })
 export class BoutiquePersonnagesComponent implements OnInit {
 
-  constructor() { }
+  personnageForm: Personnage;
+
+  constructor(private boutiquePersoService: BoutiquePersoHttpService) { }
 
   ngOnInit(): void {
   }
 
+  list(): Array<Personnage> {
+    return this.boutiquePersoService.findAll();
+  }
+
+  add() {
+    this.personnageForm = new Personnage();
+  }
+
+  edit(id: number) {
+    this.boutiquePersoService.findById(id).subscribe(response => {
+      this.personnageForm = response;
+    }, err => console.log(err));
+  }
+
+  save() {
+    if(this.personnageForm.id) {
+      this.boutiquePersoService.modify(this.personnageForm);
+    } else {
+      this.boutiquePersoService.create(this.personnageForm);
+    }
+
+    this.cancel();
+  }
+
+  cancel() {
+    this.personnageForm = null;
+  }
+
+  remove(id: number) {
+    this.boutiquePersoService.deleteById(id);
+  }
 }
